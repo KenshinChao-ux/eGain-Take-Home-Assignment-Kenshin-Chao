4d3fdba792df960608276c8c784b544d65907a8e
KenshinChao-ux <kenshinchao@gmail.com>
2025-08-15 02:51:11 -0700

    added checks to input text field for valid tracking number

--- js/index.js
+++ js/index.js
@@ -19,3 +19,24 @@ function closeChat() {
   document.getElementById("iconbtn").style.display = "block";
 }
 
+function submitTrackingId(){
+  document.getElementById('singleLineInput').readOnly = true;
+  const userInput = document.getElementById('singleLineInput').value;
+  console.log(userInput)
+  if (userInput != ''){
+    checkIfValidId(userInput);
+  }
+  
+}
+
+const noLettersRegex = /^[^a-zA-Z]*$/;
+console.log(noLettersRegex.test("")); // true
+
+function checkIfValidId(input) {
+  if (noLettersRegex.test(input) == true && input.length == 12){
+    console.log("only numbers here it works.")
+  }
+  else{
+    console.log("invalid input")
+  }
+}
